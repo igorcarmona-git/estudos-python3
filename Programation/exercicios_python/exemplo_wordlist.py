35dcbc077c7d6a672e1e78432645a69ceedc8621
Igor Carmona <igorcarmonawork@gmail.com>
2023-01-27 03:02:05 -0300

    27/01/23 03:01 --> Adicionado exemplo de wordlist e seclist

--- Programation/exercicios_python/exemplo_wordlist.py
+++ Programation/exercicios_python/exemplo_wordlist.py
@@ -1,18 +1,25 @@
 import requests
 
-alvo = "https://cianorte.pr.gov.br/"
+alvo = "https://faculdadefacec.edu.br/"
 
-wordlist = ["admin", "login", "portal", "css", "responsive"]
+# wordlist = ["admin", "login", "portal", "css", "responsive"]
+wordlist = open('prog-python/seclists/directory-list-2.3-small.txt', 'r')
+
+requisicoes_aceitas = []
 
 for word in wordlist:
   r = requests.get(alvo + word)
   print(f"Atacando --> {alvo + word}")
 
   if r.status_code == 200:
-    print("Eu tenho!")
+    print(f"REQUISIÇÃO ACEITA: {alvo + word}")
+    requisicoes_aceitas = alvo + word
+  else:
+    continue
 
-  if r.status_code != 200:
-    print(f"Não tem! {alvo+word}")
-    print(f"ERRO: {r.status_code}")
+print()
+wordlist.close()
 
-  print()
+print("LISTA DE REQUISIÇÕES QUE FORAM ACEITAS")
+for requisicao in requisicoes_aceitas:
+  print(requisicao)
