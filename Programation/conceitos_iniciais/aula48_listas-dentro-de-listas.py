9708ed772f850857b378db8eadfecd07cdf09ad9
Igor Carmona <igorcarmonawork@gmail.com>
2023-02-12 23:01:55 -0300

    12/02/2023 23:01 --> Adicionado aulas 48 e 49

---
+++ Programation/conceitos_iniciais/aula48_listas-dentro-de-listas.py
@@ -0,0 +1,25 @@
+# Lista de listas e seus índices
+
+salas = [
+    #0            1
+    ["Maria", "Helena"], #0
+    ["Elaine"], #1
+    #0         1        2               3
+    ["Luiz", "João", "Eduarda", (0, 10, 20, 30, 40)] #2]
+                                #0  1   2   3   4
+]
+
+print(salas[1][0])
+print(salas[0][1])
+print(salas[2][2])
+print(salas[2][3][2])
+
+################################# Utilizando o laço 'for' ###############################
+print("\n\n")
+
+# OBS: Interessante utilizar os dados do mesmo tipo nas listas para evitar fazer uso de 'if' complicados. 
+
+for sala in salas:
+    print(f'A Sala é {sala}')
+    for aluno in sala:
+        print(aluno)
---
+++ Programation/conceitos_iniciais/aula49_detalhes_interpretador.py
@@ -0,0 +1,37 @@
+"""
+Ambiente Virtual venv:
+    - Ambiente virtual (venv) é como se fosse uma bolha para o seu projeto. 
+    - Um ambiente virtual é uma maneira de você manter cópias do seu interpretador Python locais a uma pasta. 
+    Desta forma, você pode instalar novos pacotes de Python em pastas específicas e removê-los facilmente apenas excluindo a pasta.
+    - Virtualenvs são uma forma de isolar diversos ambientes de desenvolvimento, assim permitindo ao programador utilizar versões 
+    específicas de diversos pacotes sem impactar instalações de outras aplicações ou sistemas.
+
+Interpretador do Python:
+
+    python mod.py (executa o mod) --> exemplo: python aula33.py
+    python -u (unbuffered)
+    python -m mod (lib mod como script)
+    python -c 'cmd' (comando)
+    python -i mod.py (interativo com mod)
+
+The Zen of Python, por Tim Peters: (python -c 'import this')
+
+    Bonito é melhor que feio.
+    Explícito é melhor que implícito.
+    Simples é melhor que complexo.
+    Complexo é melhor que complicado.
+    Plano é melhor que aglomerado.
+    Esparso é melhor que denso.
+    Legibilidade conta.
+    Casos especiais não são especiais o bastante para quebrar as regras. Embora a praticidade vença a pureza.
+    Erros nunca devem passar silenciosamente.
+    A menos que sejam explicitamente silenciados.
+    Diante da ambiguidade, recuse a tentação de adivinhar.
+    Deve haver um -- e só um -- modo óbvio para fazer algo. Embora esse modo possa não ser óbvio à primeira vista a menos que você seja holandês.
+    Agora é melhor que nunca. Embora nunca frequentemente seja melhor que *exatamente* agora.
+    Se a implementação é difícil de explicar, é uma má ideia.
+    Se a implementação é fácil de explicar, pode ser uma boa ideia.
+    Namespaces são uma grande ideia -- vamos fazer mais dessas!
+"""
+# Existe ';' no python.
+print("oi"); print("oi") #Muito utilizado para executar comandos no prompt-cmd
